c9f52d9651ed6ded2d6ffc6377e85035213ca91c
AboodMokied <78992620+AboodMokied@users.noreply.github.com>
2026-02-02 15:26:53 +0200

    modify cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { validationExceptionFactory } from './common/pipes/validation.pipe';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
