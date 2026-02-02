3f280b4d9eae9062bc6f23c16c8b42f72a29e27f
AboodMokied <78992620+AboodMokied@users.noreply.github.com>
2026-02-02 16:28:53 +0200

    modify cors#2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
@@ -8,8 +9,13 @@ import { validationExceptionFactory } from './common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const corsOrigins = configService.get<string>('CORS_ORIGINS'); // EXPECT CSV string: "http://a.com,http://b.com"
+
   app.enableCors({
-    origin: '*',
+    origin: corsOrigins
+      ? corsOrigins.split(',').map((origin) => origin.trim())
+      : ['http://localhost:3000', 'https://blood-bank-app-next.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
